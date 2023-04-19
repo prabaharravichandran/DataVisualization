00f10ab7dae56b891922e52221b151bc0e0d928a
Prabahar Ravichandran <43275058+prabaharravichandran@users.noreply.github.com>
2023-04-19 14:35:56 -0600

    Change

--- Plots.R
+++ Plots.R
@@ -3,13 +3,15 @@ library(svglite)
 library(ggplot2)
 library(readxl)
 
-setwd("C:/Users/Ravichandranp/OneDrive - AGR-AGR/Documents/GitHub/DataVisualization")
-data <- read_excel("./data/WheatBreedingSampleData.xlsx", sheet = WeedScience)
 
-plt <- ggplot(mydata, aes(x, y, group = typ, color=typ)) +
-  geom_point() +
-  geom_smooth(method = 'lm', formula = y ~ x, se = FALSE) +
-  geom_errorbar(aes(ymin=y-sd, ymax=y+sd), width = 0.1) +
-  theme_minimal()
+data <- read_excel("./data/WheatBreedingSampleData.xlsx", sheet = "Breeding")
 
-ggsave('C:/Users/Ravichandranp/OneDrive - AGR-AGR/Desktop/plot.svg', plot=plt, width = 16, height = 9, units = "cm", dpi = 600, device = svglite)
+plt <- ggplot(data, aes(Yield, Maturity)) + 
+  geom_point() + 
+  geom_smooth(method='lm')
+
+
+ggsave('plot.svg', plot=plt, width = 16, height = 9, units = "cm", dpi = 600, device = svglite)
+
+
+
